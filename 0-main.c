380ec7a9f78eae1741aa13e3ad535c72ff7a8f1c
AbeebPro <mdahlat94@gmail.com>
2022-04-19 03:21:15 -0700

    Task 0

--- 0-main.c
+++ 0-main.c
@@ -2,14 +2,14 @@
 #include "main.h"
 /**
  * main - check code
- * Return: Always 0 success
+ * Return: Always 0 (success)
  */
 int main(void)
 {
 	int len, len2;
 
-	len = _printf("aabb%c%c%s%%\n", 'A', 'A', "Hello");
-	len2 = printf("aabb%c%c%s%%\n", 'A', 'A', "Hello");
+	len = _printf("aabb%c%c%s%%\n", 'A', 'B', "Hello");
+	len2 = printf("aabb%c%c%s%%\n", 'A', 'B', "Hello");
 	printf("%d\n", len);
 	printf("%d\n", len2);
 	return (0);
